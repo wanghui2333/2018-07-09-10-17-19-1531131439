44cb0868146c1175d3895748b8d83bbf6dfdcf29
wanghui <793147654@qq.com>
2019-08-23 00:07:08 +0800

    practice06 done

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Student {
+import practice06.Person;
+
+public class Student extends Person{
+
+	public int klass;
+	
+	public Student(String name, int age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String name, int age, int klass) {
+		super(name, age);
+		this.klass = klass;
+	}
+
+	public int getKlass() {
+		return klass;
+	}
+
+	public void setKlass(int klass) {
+		this.klass = klass;
+	}
+	
+	public String introduce() {
+		return super.basicIntroduce() + "I am a Student. I am at Class 2.";
+	}
+	
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,32 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+	public Integer klass;
+	
+	public Teacher(String name, int age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Teacher(String name, int age, int klass) {
+		super(name, age);
+		this.klass = klass;
+	}
+	
+	public int getKlass() {
+		return klass;
+	}
+
+	public void setKlass(int klass) {
+		this.klass = klass;
+	}
+	
+	
+	public String introduce() {
+		if (klass == null) {
+			return super.basicIntroduce() + "I am a Teacher. I teach No Class.";
+		}
+		return super.basicIntroduce() + "I am a Teacher. I teach Class 2.";
+	}
+	
 }
