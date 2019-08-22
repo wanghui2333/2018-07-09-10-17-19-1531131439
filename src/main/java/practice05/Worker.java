24745e4a89c2627eb2905523aea029ea2ced3adc
wanghui <793147654@qq.com>
2019-08-22 23:59:46 +0800

    practice05 done

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,16 @@
 package practice05;
 
-public class Worker {
+import practice05.Person;
+
+public class Worker extends Person{
+	
+	
+	public Worker(String name, int age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String introduce() {
+		return super.basicIntroduce() + "I am a Worker. I have a job.";
+	}
 }
