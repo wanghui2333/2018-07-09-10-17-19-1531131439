989080239faeb5a918cd933634527ebd3eeab48d
wanghui <793147654@qq.com>
2019-08-22 23:43:31 +0800

    practice02 done

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,27 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+
+	public int klass;
+	
+	public Student(String name, int age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String name, int age, int klass) {
+		super(name, age);
+		this.klass = klass;
+	}
+
+	public int getKlass() {
+		return klass;
+	}
+
+	public void setKlass(int klass) {
+		this.klass = klass;
+	}
+	
+	
+	
 }
